cb9c30828d30aea02a9066e72955301ccbd831e1
alkali7355 <alkali7355@gmail.com>
2025-04-28 17:43:16 +0900

    feat: 계층형 게시판 글작성 @Valid로 유효성 검사, DTO 수집 내용확인

--- src/main/java/com/miniproj/controller/BoardController.java
+++ src/main/java/com/miniproj/controller/BoardController.java
@@ -3,10 +3,12 @@ package com.miniproj.controller;
 import com.miniproj.domain.HBoardDTO;
 import com.miniproj.domain.HBoardVO;
 import com.miniproj.service.BoardService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,14 +35,23 @@ public class BoardController {
     }
 
     @GetMapping("/register")
-    public String registerGET(){
+    public String registerGET(Model model){
 
+        HBoardDTO board = new HBoardDTO();
+        model.addAttribute("board", board);
         return "/board/register";
     }
 
     @PostMapping("/register")
-    public String writeBoard( @ModelAttribute("board") HBoardDTO board){
+    public String writeBoard(@Valid @ModelAttribute("board") HBoardDTO board, BindingResult bindingResult){
+        // @Valid를 쓰면 spring이 Bean Validation호출하고 Hibernate Validator가 동작하고
+        // 그 결과를 BindingResult에 담는다.
+        if (bindingResult.hasErrors()){
+            return "/board/register";
+        }
 
+        log.info("board : {}", board);
+        return "redirect:/board/list";
     }
 
 }
--- src/main/java/com/miniproj/domain/HBoardDTO.java
+++ src/main/java/com/miniproj/domain/HBoardDTO.java
@@ -1,5 +1,7 @@
 package com.miniproj.domain;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @NoArgsConstructor
@@ -11,8 +13,14 @@ import lombok.*;
 public class HBoardDTO {
 
     private int boardNo;
+
+    @NotBlank(message = "제목은 필수 입력입니다")
+    @Size(max = 20, message = "제목은 20자 이내로 작성해 주세요")
     private String title;
+
+    @Size(max = 2000, message = "내용은 2000자 이내로 작성해 주세요")
     private String content;
+    
     private String writer;
 
 }
